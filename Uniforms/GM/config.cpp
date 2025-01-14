7d26dd7c478b70322ebffe0d98c566c92b88b33a
MikeIsTheMan <184461640+MikeIsTheMan@users.noreply.github.com>
2025-01-14 13:32:39 -0500

    Helmet and Vest Textures now working

--- Uniforms/GM/config.cpp
+++ Uniforms/GM/config.cpp
@@ -1,4 +1,4 @@
-/*class CfgPatches
+class CfgPatches
 {
     class 332nd_Custom_Mod
     {
@@ -30,38 +30,19 @@ class CfgWeapons
         author = "Custom";
         displayName = "[332nd] Marine Helmet";
         scope = 2;
-        hiddenSelectionsTextures[] =
-        {
-            "\332nd_Aux\Helmets\Tex\332nd_Marine_Helmet_CT.paa",
-            "\JLTS_AE_GM\data\BaseTextures\BaseHelmet\Visor_co.paa"
-        };
+        hiddenSelections[]=
+		{
+			"Camo1",
+			"Visor"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\332nd_Aux\Helmets\Tex\332nd_Marine_Helmet_CT.paa",
+			"\JLTS_AE_GM\data\ReTextures\21st\Helmet.paa"
+		};
     };
 
-    // Engineer Helmet
-    class 332nd_Marine_Engineer_Helmet: SEA_Helmet_GM_21_Engineer
-    {
-        author = "Custom";
-        displayName = "[332nd] Marine Engineer Helmet";
-        scope = 2;
-        hiddenSelectionsTextures[] =
-        {
-            "\332nd_Aux\Helmets\Tex\332nd_Marine_Engineer_Helmet.paa",
-            "\JLTS_AE_GM\data\BaseTextures\BaseHelmet\Visor_co.paa"
-        };
-    };
 
-    // RTO Helmet
-    class 332nd_Marine_RTO_Helmet: SEA_Helmet_GM_21_RTO
-    {
-        author = "Custom";
-        displayName = "[332nd] Marine RTO Helmet";
-        scope = 2;
-        hiddenSelectionsTextures[] =
-        {
-            "\332nd_Aux\Helmets\Tex\332nd_Marine_RTO_Helmet.paa",
-            "\JLTS_AE_GM\data\BaseTextures\BaseHelmet\Visor_co.paa"
-        };
-    };
 
     // Marine Vest
     class 332nd_Marine_Vest: SEA_Vest_GM_21
@@ -69,12 +50,10 @@ class CfgWeapons
         author = "Custom";
         displayName = "[332nd] Marine Vest";
         scope = 2;
-        hiddenSelectionsTextures[] =
-        {
-            "\332nd_Aux\Vests\Tex\332nd_Marine_Vest.paa",
-            "\JLTS_AE_GM\data\BaseTextures\Vest\Vest_Lower_co.paa",
-            "\JLTS_AE_GM\data\BaseTextures\Vest\Vest_Upper_co.paa"
-        };
+		hiddenSelectionsTextures[]=
+		{
+			"\332nd_Aux\Vests\Tex\332nd_Marine_Vest.paa"
+		};
     };
 
     // Marine Uniform
@@ -85,7 +64,7 @@ class CfgWeapons
         scope = 2;
         class ItemInfo
         {
-            uniformClass = "332nd_Marine_Uniform_Veh";
+            uniformClass = "SEA_Uniform_GM_Plastic_21";
         };
         hiddenSelectionsTextures[] =
         {
@@ -98,45 +77,4 @@ class CfgWeapons
 
 class CfgVehicles
 {
-    class SEA_Backpack_GM_Base;
-    class SEA_Backpack_GM_Medic;
-    class SEA_Backpack_GM_RTO;
-
-    // Marine Backpack
-    class 332nd_Marine_Backpack: SEA_Backpack_GM_Base
-    {
-        author = "Custom";
-        displayName = "[332nd] Marine Backpack";
-        scope = 2;
-        hiddenSelectionsTextures[] =
-        {
-            "\332nd_Aux\Backpacks\Tex\332nd_Marine_Backpack.paa"
-        };
-    };
-
-    // Medic Backpack
-    class 332nd_Marine_Medic_Backpack: SEA_Backpack_GM_Medic
-    {
-        author = "Custom";
-        displayName = "[332nd] Marine Medic Backpack";
-        scope = 2;
-        hiddenSelectionsTextures[] =
-        {
-            "\332nd_Aux\Backpacks\Tex\332nd_Marine_Medic_Backpack.paa"
-        };
-    };
-
-    // RTO Backpack
-    class 332nd_Marine_RTO_Backpack: SEA_Backpack_GM_RTO
-    {
-        author = "Custom";
-        displayName = "[332nd] Marine RTO Backpack";
-        scope = 2;
-        hiddenSelectionsTextures[] =
-        {
-            "\332nd_Aux\Backpacks\Tex\332nd_Marine_RTO_Backpack.paa",
-            "\JLTS_AE_GM\data\BaseTextures\Backpack\RTO\Backpack_RTO_ca.paa"
-        };
-    };
 };
-*/
