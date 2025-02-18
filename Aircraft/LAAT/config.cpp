e57847ffce08b783e9f97485859ab1a3c64f95d9
Cherryy <Ross_Nichols22@Outlook.com>
2025-02-18 02:33:14 -0600

    LAAT hotfix and ARF Camo bags
    Includes Backpack / JTAC / Medic / & Heavy

--- Aircraft/LAAT/config.cpp
+++ Aircraft/LAAT/config.cpp
@@ -572,7 +572,7 @@ class CfgVehicles
 					"lsd_vehicles_heli\laati\data\332nd_Ikran_Body2.paa",
 					"332nd_Aux\Aircraft\LAAT\Tex\332nd_Ikran_Doors1.paa",
 					"332nd_Aux\Aircraft\LAAT\Tex\332nd_Ikran_Doors2.paa",
-					"332nd_Aux\Aircraft\LAAT\Tex\332nd_Ikran_Doors2.paa",
+					"332nd_Aux\Aircraft\LAAT\Tex\332nd_Ikran_Doors3.paa",
 					"332nd_Aux\Aircraft\LAAT\Tex\332nd_Ikran_Wings.paa",
 					"lsd_vehicles_heli\laati\data\missiles_co.paa",
 					"lsd_vehicles_heli\laati\data\cockpits_co.paa",
