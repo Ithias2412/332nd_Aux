b7b2869c881e675c11049cdecb364b32d6a86a6b
Ithias <ALDieguez9@gmail.com>
2025-02-20 19:46:13 -0600

    Music QA

--- Music/config.cpp
+++ Music/config.cpp
@@ -13,238 +13,154 @@ class CfgPatches
 };
 class CfgMusic
 {
+	//Rock
     class Brain_Stew
     {
         name = "Brain Stew";
-		sound[] = {"332nd_Aux\music\music\brain_Stew.ogg",1,1};
+		sound[] = {"332nd_Aux\music\music\Rock\brain_Stew.ogg",1,1};
 		duration = 190;
-		musicClass = "332nd_Music";
+		musicClass = "332nd_Music_Rock";
     };
     class Highway_to_Hell
     {
         name = "Highway To Hell";
-		sound[] = {"332nd_Aux\music\music\Highway_to_Hell.ogg",1,1};
+		sound[] = {"332nd_Aux\music\music\Rock\Highway_to_Hell.ogg",1,1};
 		duration = 190;
-		musicClass = "332nd_Music";
+		musicClass = "332nd_Music_Rock";
     };
     class Sympathy_for_the_Devil
     {
         name = "Sympathy for the Devil";
-		sound[] = {"332nd_Aux\music\music\Sympathy_for_the_Devil.ogg",1,1};
+		sound[] = {"332nd_Aux\music\music\Rock\Sympathy_for_the_Devil.ogg",1,1};
 		duration = 190;
-		musicClass = "332nd_Music";
+		musicClass = "332nd_Music_Rock";
     };
     class Thunderstruck
     {
         name = "Thunderstruck";
-		sound[] = {"332nd_Aux\music\music\Thunderstruck.ogg",1,1};
+		sound[] = {"332nd_Aux\music\music\Rock\Thunderstruck.ogg",1,1};
 		duration = 190;
-		musicClass = "332nd_Music";
+		musicClass = "332nd_Music_Rock";
     };
     class Welcome_to_the_Jungle
     {
         name = "Welcome to the Jungle";
-		sound[] = {"332nd_Aux\music\music\Welcome_to_the_Jungle.ogg",1,1};
+		sound[] = {"332nd_Aux\music\music\Rock\Welcome_to_the_Jungle.ogg",1,1};
 		duration = 190;
-		musicClass = "332nd_Music";
+		musicClass = "332nd_Music_Rock";
     };
 	//Project Wingman
-	class PW_Active_Contract
-    {
-        name = "(PW) Active Contract";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) Active Contract.ogg",1,1};
-		duration = 125;
-		musicClass = "332nd_Music";
-    };
 	class PW_Calamity
     {
-        name = "(PW) Calamity";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) Calamity.ogg",1,1};
-		duration = 122;
-		musicClass = "332nd_Music";
-    };
-	class PW_Clear_Skies
-    {
-        name = "(PW) Clear Skies";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) Clear Skies.ogg",1,1};
-		duration = 122;
-		musicClass = "332nd_Music";
-    };
-	class PW_Consequence_of_Power
-    {
-        name = (PW) "Consequence of Power";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) Consequence of Power.ogg",1,1};
+        name = "Calamity";
+		sound[] = {"332nd_Aux\music\music\Project_Wingman\Calamity.ogg",1,1};
 		duration = 123;
-		musicClass = "332nd_Music";
-    };
-	class PW_Contract_Details
-    {
-        name = "(PW) Contract Details";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) Consequence of Power.ogg",1,1};
-		duration = 92;
-		musicClass = "332nd_Music";
-    };
-	class PW_Coronation
-    {
-        name = "(PW) Coronation";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) Coronation.ogg",1,1};
-		duration = 144;
-		musicClass = "332nd_Music";
-    };
-	class PW_Daedalus
-    {
-        name = "(PW) Daedalus";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) Daedalus.ogg",1,1};
-		duration = 182;
-		musicClass = "332nd_Music";
-    };
-	class PW_Danger_Bound
-    {
-        name = "(PW) Danger Bound";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) Danger Bound.ogg",1,1};
-		duration = 194;
-		musicClass = "332nd_Music";
-    };
-	class PW_Eminent_Domain
-    {
-        name = "(PW) Eminent Domain";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) Eminent Domain.ogg",1,1};
-		duration = 191;
-		musicClass = "332nd_Music";
-    };
-	class PW_End_Credits
-    {
-        name = "(PW) End Credits";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) End Credits.ogg",1,1};
-		duration = 206;
-		musicClass = "332nd_Music";
-    };
-	class PW_Endless
-    {
-        name = "(PW) Endless";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) Endless.ogg",1,1};
-		duration = 192;
-		musicClass = "332nd_Music";
-    };
-	class PW_Falling_Skies
-    {
-        name = "(PW) Falling Skies";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) Falling Skies.ogg",1,1};
-		duration = 145;
-		musicClass = "332nd_Music";
-    };
-	class PW_Fire_and_Dust
-    {
-        name = "(PW) Fire and Dust";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) Fire and Dust.ogg",1,1};
-		duration = 231;
-		musicClass = "332nd_Music";
-    };
-	class PW_Fire_with_Fire
-    {
-        name = "(PW) Fire with Fire";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) Fire with Fire.ogg",1,1};
-		duration = 150;
-		musicClass = "332nd_Music";
-    };
-	class PW_Golden_Age
-    {
-        name = "(PW) Golden Age";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) Golden Age.ogg",1,1};
-		duration = 94;
-		musicClass = "332nd_Music";
-    };
-	class PW_Hitmen
-    {
-        name = "(PW) Hitmen";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) Hitmen.ogg",1,1};
-		duration = 301;
-		musicClass = "332nd_Music";
+		musicClass = "332nd_Music_PW";
     };
 	class PW_Kings_Court
     {
-        name = "(PW) Kings Court";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) Kings Court.ogg",1,1};
-		duration = 155;
-		musicClass = "332nd_Music";
+        name = "Kings Court";
+		sound[] = {"332nd_Aux\music\music\Project_Wingman\Kings_Court.ogg",1,1};
+		duration = 178;
+		musicClass = "332nd_Music_PW";
     };
 	class PW_Kings
     {
-        name = "(PW) Kings";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) Kings.ogg",1,1};
-		duration = 704;
-		musicClass = "332nd_Music";
-    };
-	class PW_Landing
-    {
-        name = "(PW) Landing";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) Landing.ogg",1,1};
-		duration = 71;
-		musicClass = "332nd_Music";
+        name = "Kings";
+		sound[] = {"332nd_Aux\music\music\Project_Wingman\Kings.ogg",1,1};
+		duration = 424;
+		musicClass = "332nd_Music_PW";
     };
 	class PW_Magma_Carta
     {
-        name = "(PW) Magma Carta";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) Magma Carta.ogg",1,1};
-		duration = 184;
-		musicClass = "332nd_Music";
+        name = "Magma Carta";
+		sound[] = {"332nd_Aux\music\music\Project_Wingman\Magma_Carta.ogg",1,1};
+		duration = 187;
+		musicClass = "332nd_Music_PW";
     };
 	class PW_Midnight_Light
     {
-        name = "(PW) Midnight Light";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) Midnight Light.ogg",1,1};
-		duration = 187;
-		musicClass = "332nd_Music";
+        name = "Midnight Light";
+		sound[] = {"332nd_Aux\music\music\Project_Wingman\Midnight_Light.ogg",1,1};
+		duration = 191;
+		musicClass = "332nd_Music_PW";
     };
 	class PW_Peacekeeper_II
     {
-        name = "(PW) Peacekeeper II";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) Peacekeeper II.ogg",1,1};
-		duration = 151;
-		musicClass = "332nd_Music";
+        name = "Peacekeeper II";
+		sound[] = {"332nd_Aux\music\music\Project_Wingman\Peacekeeper_II.ogg",1,1};
+		duration = 172;
+		musicClass = "332nd_Music_PW";
     };
 	class PW_Peacekeeper
     {
-        name = "(PW) Peacekeeper";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) Peacekeeper.ogg",1,1};
-		duration = 305;
-		musicClass = "332nd_Music";
+        name = "Peacekeeper";
+		sound[] = {"332nd_Aux\music\music\Project_Wingman\Peacekeeper.ogg",1,1};
+		duration = 185;
+		musicClass = "332nd_Music_PW";
     };
 	class PW_Presidia
     {
-        name = "(PW) Presidia";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) Presidia.ogg",1,1};
-		duration = 311;
-		musicClass = "332nd_Music";
+        name = "Presidia";
+		sound[] = {"332nd_Aux\music\music\Project_Wingman\Presidia.ogg",1,1};
+		duration = 318;
+		musicClass = "332nd_Music_PW";
     };
-	class PW_Pyroclastic
+	class PW_Professional_Execution
     {
-        name = "(PW) Pyroclastic";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) Pyroclastic.ogg",1,1};
-		duration = 185;
-		musicClass = "332nd_Music";
+        name = "Professional Execution";
+		sound[] = {"332nd_Aux\music\music\Project_Wingman\Professional_Execution.ogg",1,1};
+		duration = 153;
+		musicClass = "332nd_Music_PW";
     };
-	class PW_Red_Sea
+	class PW_Showdown
     {
-        name = "(PW) Red Sea";
-		sound[] = {"332nd_Aux\music\music\Project Wingman\(PW) Red Sea.ogg",1,1};
-		duration = 184;
-		musicClass = "332nd_Music";
+        name = "Showdown";
+		sound[] = {"332nd_Aux\music\music\Project_Wingman\Showdown.ogg",1,1};
+		duration = 189;
+		musicClass = "332nd_Music_PW";
     };
-	//Republic Commando
+
+	//Misc
 	class Republic_Commando_Vode_An
     {
-        name = "Republic Commando Vode An";
-		sound[] = {"332nd_Aux\music\music\Republic Commando Vode An.ogg",1,1};
+        name = "Republic Commando - Vode An";
+		sound[] = {"332nd_Aux\music\music\Misc\Republic Commando Vode An.ogg",1,1};
 		duration = 184;
-		musicClass = "332nd_Music";
+		musicClass = "332nd_Music_Misc";
     };
+	class HIKARI
+    {
+        name = "HIKARI";
+		sound[] = {"332nd_Aux\music\music\Misc\HIKARI.ogg",1,1};
+		duration = 112;
+		musicClass = "332nd_Music_Misc";
+    };
+	class November_Rain_Ending
+    {
+        name = "November Rain Ending";
+		sound[] = {"332nd_Aux\music\music\Misc\November_Rain_Ending.ogg",1,1};
+		duration = 126;
+		musicClass = "332nd_Music_Misc";
+    };
+
 };
+
+
 
 class CfgMusicClasses
 {
-	class 332nd_Music
+	class 332nd_Music_PW
 	{
-		displayName = "[332nd] Music";
+		displayName = "[332nd] Project Wingman";
 	};
+	class 332nd_Music_Rock
+	{
+		displayName = "[332nd] Rock";
+	};
+	class 332nd_Music_Misc
+	{
+		displayName = "[332nd] Misc";
+	};
+
 };
