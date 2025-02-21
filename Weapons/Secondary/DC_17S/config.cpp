6fd39af1e1e1af74fa1f04c1e5254811275be457
MikeIsTheMan <184461640+MikeIsTheMan@users.noreply.github.com>
2025-02-20 20:36:35 -0500

    added stun rounds that don't work?????

--- Weapons/Secondary/DC_17S/config.cpp
+++ Weapons/Secondary/DC_17S/config.cpp
@@ -36,7 +36,8 @@ class cfgWeapons
 		magazines[] =
 		{
 			"332nd_DC17S_Mag",
-			"332nd_DC17S_45ACP_Mag"
+			"332nd_DC17S_45ACP_Mag",
+			"ls_mag_stunLow_2Rnd"
 		};
 		magazineWell[]=
 		{
@@ -50,7 +51,7 @@ class cfgWeapons
 	class 33nd_DC17S_Dual : 3AS_DC17STest_F
 	{
 		displayName = "[332] Dual DC-17S Sidearm";
-		magazines[] = {"3AS_60Rnd_Test_mag"};
+		magazines[] = {"3AS_60Rnd_Test_mag","ls_mag_stunLow_2Rnd"};
 		recoil = "3as_recoil_light";
 
 	};
