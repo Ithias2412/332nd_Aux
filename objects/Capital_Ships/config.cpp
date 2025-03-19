fd91fa58d07b7e3dcdce02748503eec56d225986
Cherryy <Ross_Nichols22@Outlook.com>
2025-03-18 21:16:19 -0500

    Custom 332nd Venator
    Use for whatever

---
+++ objects/Capital_Ships/config.cpp
@@ -0,0 +1,715 @@
+class CfgPatches
+{
+	class 332nd_Shili_Moon
+	{
+		author="Cherryy";
+		name="";
+		addonRootClass="";
+		requiredVersion=2.1600001;
+		requiredAddons[]=
+		{
+			"ls_venator",
+		};
+		units[]=
+		{
+			"332nd_Venator_Body1_1",
+			"332nd_Venator_Body1_2",
+			"332nd_Venator_Body2_1",
+			"332nd_Venator_Body2_2",
+			"332nd_Venator_Body2_3",
+			"332nd_Venator_Body3",
+			"332nd_Venator_Body4",
+			"332nd_Venator_Body5",
+			"332nd_Venator_Body6",
+			"332nd_Venator_Body7",
+			"332nd_Venator_Body8",
+			"332nd_Venator_Body9",
+			"332nd_Venator_Body10",
+			"332nd_Venator_Bridge",
+			"332nd_Venator_Door",
+			"332nd_Venator_Door2",
+			"332nd_Venator_Engine",
+			"332nd_Venator_Interior1",
+			"332nd_Venator_Interior2",
+			"332nd_Venator_Interior3",
+			"332nd_Venator_Interior4",
+			"332nd_Venator_Interior5",
+			"332nd_Venator_Interior6",
+			"332nd_Venator_Interior7",
+			"332nd_Venator_Interior8",
+			"332nd_Venator_ClosedFullyHollowed",
+			"332nd_Venator_OpenFullyHollowed",
+			"332nd_Venator_Zeus"
+		};
+		weapons[]={};
+		version=2.45;
+		versionStr="2.45.5.0";
+		versionAr[]={2,45,5,0};
+	};
+};
+class CfgEditorCategories
+{
+    class 332nd_Ship_Subcategory 
+	{
+        displayName = "[332nd] Capital Ships";  // This will appear in the Eden Editor under the Units section
+    };
+    class 332nd_Objects_Category 
+	{
+		displayName = "[332nd] Objects"; // Name visible in the list
+	};
+};
+class CfgVehicles
+{
+    //base classes
+    class venator_base;
+    class venator_base_5;
+	class ven_body1_1;
+    class ven_body1_2;
+    class ven_body2_1;
+    class ven_body2_2;
+    class ven_body2_3;
+    class ven_body3;
+    class ven_body4;
+    class ven_body5;
+    class ven_body6;
+    class ven_body7;
+    class ven_body8;
+    class ven_body9;
+    class ven_body10;
+    class ven_bridge;
+    class ven_door;
+    class ven_door2;
+    class ven_engine;
+    class ven_interior1;
+    class ven_interior2;
+    class ven_interior3;
+    class ven_interior4;
+    class ven_interior5;
+    class ven_interior6;
+    class ven_interior7;
+    class ven_interior8;
+    class venator_zeus;
+	class 332nd_Venator_Body1_1: ven_body1_1
+	{
+		scope=1;
+		scopeArsenal=0;
+		scopeCurator=0;
+		displayName="[332nd] Venator Body 1-1";
+		hiddenSelectionsTextures[]=
+		{
+			"332nd_Aux\objects\tex\Venator\Body1_co.paa"
+		};
+	};
+	class 332nd_Venator_Body1_2: ven_body1_2
+	{
+		scope=1;
+		scopeArsenal=0;
+		scopeCurator=0;
+		displayName="[332nd] Venator Body 1-2";
+		hiddenSelectionsTextures[]=
+		{
+			"332nd_Aux\objects\tex\Venator\Body1_co.paa"
+		};
+	};
+	class 332nd_Venator_Body2_1: ven_body2_1
+	{
+		scope=1;
+		scopeArsenal=0;
+		scopeCurator=0;
+		displayName="[332nd] Venator Body 2-1";
+		hiddenSelectionsTextures[]=
+		{
+			"332nd_Aux\objects\tex\Venator\Body2_co.paa"
+		};
+	};
+	class 332nd_Venator_Body2_2: ven_body2_2
+	{
+		scope=1;
+		scopeArsenal=0;
+		scopeCurator=0;
+		displayName="[332nd] Venator Body 2-2";
+		hiddenSelectionsTextures[]=
+		{
+			"332nd_Aux\objects\tex\Venator\Body2_co.paa"
+		};
+	};
+	class 332nd_Venator_Body2_3: ven_body2_3
+	{
+		scope=1;
+		scopeArsenal=0;
+		scopeCurator=0;
+		displayName="[332nd] Venator Body 2-3";
+		hiddenSelectionsTextures[]=
+		{
+			"332nd_Aux\objects\tex\Venator\Body2_co.paa"
+		};
+	};
+	class 332nd_Venator_Body3: ven_body3
+	{
+		scope=1;
+		scopeArsenal=0;
+		scopeCurator=0;
+		displayName="[332nd] Venator Body 3";
+		hiddenSelectionsTextures[]=
+		{
+			"332nd_Aux\objects\tex\Venator\Body3_co.paa"
+		};
+	};
+	class 332nd_Venator_Body4: ven_body4
+	{
+		scope=1;
+		scopeArsenal=0;
+		scopeCurator=0;
+		displayName="[332nd] Venator Body 4";
+		hiddenSelectionsTextures[]=
+		{
+			"332nd_Aux\objects\tex\Venator\Body4_co.paa"
+		};
+	};
+	class 332nd_Venator_Body5: ven_body5
+	{
+		scope=1;
+		scopeArsenal=0;
+		scopeCurator=0;
+		displayName="[332nd] Venator Body 5";
+		hiddenSelectionsTextures[]=
+		{
+			"332nd_Aux\objects\tex\Venator\Body5_co.paa"
+		};
+	};
+	class 332nd_Venator_Body6: ven_body6
+	{
+		scope=1;
+		scopeArsenal=0;
+		scopeCurator=0;
+		displayName="[332nd] Venator Body 6";
+		hiddenSelectionsTextures[]=
+		{
+			"332nd_Aux\objects\tex\Venator\Body6_co.paa"
+		};
+	};
+	class 332nd_Venator_Body7: ven_body7
+	{
+		scope=1;
+		scopeArsenal=0;
+		scopeCurator=0;
+		displayName="[332nd] Venator Body 7";
+		hiddenSelectionsTextures[]=
+		{
+			"332nd_Aux\objects\tex\Venator\Body7_co.paa"
+		};
+	};
+	class 332nd_Venator_Body8: ven_body8
+	{
+		scope=1;
+		scopeArsenal=0;
+		scopeCurator=0;
+		displayName="[332nd] Venator Body 8";
+		hiddenSelectionsTextures[]=
+		{
+			"332nd_Aux\objects\tex\Venator\Body8_co.paa"
+		};
+	};
+	class 332nd_Venator_Body9: ven_body9
+	{
+		scope=1;
+		scopeArsenal=0;
+		scopeCurator=0;
+		displayName="[332nd] Venator Body 9";
+		hiddenSelectionsTextures[]=
+		{
+			"332nd_Aux\objects\tex\Venator\Body9_co.paa"
+		};
+	};
+	class 332nd_Venator_Body10: ven_body10
+	{
+		scope=1;
+		scopeArsenal=0;
+		scopeCurator=0;
+		displayName="[332nd] Venator Body 10";
+		hiddenSelectionsTextures[]=
+		{
+			"332nd_Aux\objects\tex\Venator\Body10_co.paa"
+		};
+	};
+	class 332nd_Venator_Bridge: ven_bridge
+	{
+		scope=1;
+		scopeArsenal=0;
+		scopeCurator=0;
+		displayName="[332nd] Venator Bridge";
+		hiddenSelectionsTextures[]=
+		{
+			"332nd_Aux\objects\tex\Venator\Bridge_co.paa"
+		};
+	};
+	class 332nd_Venator_Door: ven_door
+	{
+		scope=1;
+		scopeArsenal=1;
+		scopeCurator=1;
+		editorCategory="332nd_Objects_Category";
+		editorSubcategory="332nd_Ship_Subcategory";
+		displayName="[332nd] Venator Door (Closed)";
+		hiddenSelectionsTextures[]=
+		{
+			"332nd_Aux\objects\tex\Venator\Door_co.paa"
+		};
+	};
+	class 332nd_Venator_Door2: ven_door2
+	{
+		scope=1;
+		scopeArsenal=1;
+		scopeCurator=1;
+		editorCategory="332nd_Objects_Category";
+		editorSubcategory="332nd_Ship_Subcategory";
+		displayName="[332nd] Venator Door (Open)";
+		hiddenSelectionsTextures[]=
+		{
+			"",
+			"332nd_Aux\objects\tex\Venator\Door_co.paa"
+		};
+	};
+	class 332nd_Venator_Engine: ven_engine
+	{
+		scope=1;
+		scopeArsenal=0;
+		scopeCurator=0;
+		displayName="[332nd] Venator Engine";
+		hiddenSelectionsTextures[]=
+		{
+			"332nd_Aux\objects\tex\Venator\Engine_co.paa"
+		};
+	};
+	class 332nd_Venator_Interior1: ven_interior1
+	{
+		scope=1;
+		scopeArsenal=0;
+		scopeCurator=0;
+		displayName="[332nd] Venator Interior 1";
+		hiddenSelectionsTextures[]=
+		{
+			"332nd_Aux\objects\tex\Venator\Interior1_co.paa"
+		};
+	};
+	class 332nd_Venator_Interior2: ven_interior2
+	{
+		scope=1;
+		scopeArsenal=0;
+		scopeCurator=0;
+		displayName="[332nd] Venator Interior 2";
+		hiddenSelectionsTextures[]=
+		{
+			"332nd_Aux\objects\tex\Venator\Interior2_co.paa"
+		};
+	};
+	class 332nd_Venator_Interior3: ven_interior3
+	{
+		scope=1;
+		scopeArsenal=0;
+		scopeCurator=0;
+		displayName="[332nd] Venator Interior 3";
+		hiddenSelectionsTextures[]=
+		{
+			"332nd_Aux\objects\tex\Venator\Interior3_co.paa"
+		};
+	};
+	class 332nd_Venator_Interior4: ven_interior4
+	{
+		scope=1;
+		scopeArsenal=0;
+		scopeCurator=0;
+		displayName="[332nd] Venator Interior 4";
+		hiddenSelectionsTextures[]=
+		{
+			"332nd_Aux\objects\tex\Venator\Interior4_co.paa"
+		};
+	};
+	class 332nd_Venator_Interior5: ven_interior5
+	{
+		scope=1;
+		scopeArsenal=0;
+		scopeCurator=0;
+		displayName="[332nd] Venator Interior 5";
+		hiddenSelectionsTextures[]=
+		{
+			"332nd_Aux\objects\tex\Venator\Interior5_co.paa"
+		};
+	};
+	class 332nd_Venator_Interior6: ven_interior6
+	{
+		scope=1;
+		scopeArsenal=0;
+		scopeCurator=0;
+		displayName="[332nd] Venator Interior 6";
+		hiddenSelectionsTextures[]=
+		{
+			"332nd_Aux\objects\tex\Venator\Interior6_co.paa"
+		};
+	};
+	class 332nd_Venator_Interior7: ven_interior7
+	{
+		scope=1;
+		scopeArsenal=0;
+		scopeCurator=0;
+		displayName="[332nd] Venator Interior 7";
+		hiddenSelectionsTextures[]=
+		{
+			"332nd_Aux\objects\tex\Venator\Interior7_co.paa"
+		};
+	};
+	class 332nd_Venator_Interior8: ven_interior8
+	{
+		scope=1;
+		scopeArsenal=0;
+		scopeCurator=0;
+		displayName="[332nd] Venator Interior 8";
+		hiddenSelectionsTextures[]=
+		{
+			"332nd_Aux\objects\tex\Venator\Interior8_co.paa"
+		};
+	};
+	class 332nd_Venator_ClosedFullyHollowed: venator_base_5
+	{
+		scope=2;
+		scopeArsenal=2;
+		scopeCurator=2;
+		author="Cherryy";
+		editorCategory="332nd_Objects_Category";
+		editorSubcategory="332nd_Ship_Subcategory";
+		displayName="[332nd] Venator (Closed Fully Hollowed)";
+		multistructureparts[]=
+		{
+			
+			{
+				"332nd_Venator_Body1_1",
+				"body1_1"
+			},
+			
+			{
+				"332nd_Venator_Body1_2",
+				"body1_2"
+			},
+			
+			{
+				"332nd_Venator_Body2_1",
+				"body2_1"
+			},
+			
+			{
+				"332nd_Venator_Body2_2",
+				"body2_2"
+			},
+			
+			{
+				"332nd_Venator_Body2_3",
+				"body2_3"
+			},
+			
+			{
+				"332nd_Venator_Body3",
+				"body3"
+			},
+			
+			{
+				"332nd_Venator_Body4",
+				"body4"
+			},
+			
+			{
+				"332nd_Venator_Body5",
+				"body5"
+			},
+			
+			{
+				"332nd_Venator_Body6",
+				"body6"
+			},
+			
+			{
+				"332nd_Venator_Body7",
+				"body7"
+			},
+			
+			{
+				"332nd_Venator_Body8",
+				"body8"
+			},
+			
+			{
+				"332nd_Venator_Body9",
+				"body9"
+			},
+			
+			{
+				"332nd_Venator_Body10",
+				"body10"
+			},
+			
+			{
+				"332nd_Venator_Door",
+				"door"
+			},
+			
+			{
+				"332nd_Venator_Engine",
+				"engine"
+			},
+			
+			{
+				"332nd_Venator_Bridge",
+				"bridge"
+			}
+		};
+	};
+	class 332nd_Venator_OpenFullyHollowed: 332nd_Venator_ClosedFullyHollowed
+	{
+		displayName="[332nd] Venator (Open Fully Hollowed)";
+		multiStructureParts[]=
+		{
+			
+			{
+				"332nd_Venator_Body1_1",
+				"body1_1"
+			},
+			
+			{
+				"332nd_Venator_Body1_2",
+				"body1_2"
+			},
+			
+			{
+				"332nd_Venator_Body2_1",
+				"body2_1"
+			},
+			
+			{
+				"332nd_Venator_Body2_2",
+				"body2_2"
+			},
+			
+			{
+				"332nd_Venator_Body2_3",
+				"body2_3"
+			},
+			
+			{
+				"332nd_Venator_Body3",
+				"body3"
+			},
+			
+			{
+				"332nd_Venator_Body4",
+				"body4"
+			},
+			
+			{
+				"332nd_Venator_Body5",
+				"body5"
+			},
+			
+			{
+				"332nd_Venator_Body6",
+				"body6"
+			},
+			
+			{
+				"332nd_Venator_Body7",
+				"body7"
+			},
+			
+			{
+				"332nd_Venator_Body8",
+				"body8"
+			},
+			
+			{
+				"332nd_Venator_Body9",
+				"body9"
+			},
+			
+			{
+				"332nd_Venator_Body10",
+				"body10"
+			},
+			
+			{
+				"332nd_Venator_Door2",
+				"door"
+			},
+			
+			{
+				"332nd_Venator_Engine",
+				"engine"
+			},
+			
+			{
+				"332nd_Venator_Bridge",
+				"bridge"
+			}
+		};
+	};
+	class 332nd_Venator_Zeus: venator_zeus
+	{
+		scope=2;
+		scopeArsenal=2;
+		scopeCurator=2;
+		author="Cherryy";
+		editorCategory="332nd_Objects_Category";
+		editorSubcategory="332nd_Ship_Subcategory";
+		displayName="[332nd] Venator (Zeus)";
+		hiddenSelectionsTextures[]=
+		{
+			"332nd_Aux\objects\tex\Venator\Body1_co.paa",
+			"332nd_Aux\objects\tex\Venator\Body2_co.paa",
+			"332nd_Aux\objects\tex\Venator\Body3_co.paa",
+			"332nd_Aux\objects\tex\Venator\Body4_co.paa",
+			"332nd_Aux\objects\tex\Venator\Body5_co.paa",
+			"332nd_Aux\objects\tex\Venator\Body6_co.paa",
+			"332nd_Aux\objects\tex\Venator\Body7_co.paa",
+			"332nd_Aux\objects\tex\Venator\Body8_co.paa",
+			"332nd_Aux\objects\tex\Venator\Body9_co.paa",
+			"332nd_Aux\objects\tex\Venator\Body10_co.paa",
+			"332nd_Aux\objects\tex\Venator\Door_co.paa",
+			"332nd_Aux\objects\tex\Venator\Engine_co.paa",
+			"332nd_Aux\objects\tex\Venator\Interior1_co.paa",
+			"332nd_Aux\objects\tex\Venator\Interior2_co.paa",
+			"332nd_Aux\objects\tex\Venator\Interior3_co.paa",
+			"332nd_Aux\objects\tex\Venator\Interior4_co.paa",
+			"332nd_Aux\objects\tex\Venator\Interior5_co.paa",
+			"332nd_Aux\objects\tex\Venator\Interior6_co.paa",
+			"332nd_Aux\objects\tex\Venator\Interior7_co.paa",
+			"332nd_Aux\objects\tex\Venator\Interior8_co.paa",
+			"332nd_Aux\objects\tex\Venator\Bridge_co.paa"
+		};
+	};
+    class 332nd_Venator_base_2: venator_base
+	{
+        editorCategory="332nd_Objects_Category";
+		editorSubcategory="332nd_Ship_Subcategory";
+		displayname="[332nd] Venator (Open)";
+		multistructureparts[]=
+		{
+			
+			{
+				"332nd_Venator_Body1_1",
+				"body1_1"
+			},
+			
+			{
+				"332nd_Venator_Body1_2",
+				"body1_2"
+			},
+			
+			{
+				"332nd_Venator_Body2_1",
+				"body2_1"
+			},
+			
+			{
+				"332nd_Venator_Body2_2",
+				"body2_2"
+			},
+			
+			{
+				"332nd_Venator_Body2_3",
+				"body2_3"
+			},
+			
+			{
+				"332nd_Venator_Body3",
+				"body3"
+			},
+			
+			{
+				"332nd_Venator_Body4",
+				"body4"
+			},
+			
+			{
+				"332nd_Venator_Body5",
+				"body5"
+			},
+			
+			{
+				"332nd_Venator_Body6",
+				"body6"
+			},
+			
+			{
+				"332nd_Venator_Body7",
+				"body7"
+			},
+			
+			{
+				"332nd_Venator_Body8",
+				"body8"
+			},
+			
+			{
+				"332nd_Venator_Body9",
+				"body9"
+			},
+			
+			{
+				"332nd_Venator_Body10",
+				"body10"
+			},
+			
+			{
+				"332nd_Venator_Door2",
+				"door"
+			},
+			
+			{
+				"332nd_Venator_Engine",
+				"engine"
+			},
+			
+			{
+				"332nd_Venator_Interior1",
+				"interior1"
+			},
+			
+			{
+				"332nd_Venator_Interior2",
+				"interior2"
+			},
+			
+			{
+				"332nd_Venator_Interior3",
+				"interior3"
+			},
+			
+			{
+				"332nd_Venator_Interior4",
+				"interior4"
+			},
+			
+			{
+				"332nd_Venator_Interior5",
+				"interior5"
+			},
+			
+			{
+				"332nd_Venator_Interior6",
+				"interior6"
+			},
+			
+			{
+				"332nd_Venator_Interior7",
+				"interior7"
+			},
+			
+			{
+				"332nd_Venator_Interior8",
+				"interior8"
+			},
+			
+			{
+				"332nd_Venator_Bridge",
+				"bridge"
+			}
+		};
+	};
+};
