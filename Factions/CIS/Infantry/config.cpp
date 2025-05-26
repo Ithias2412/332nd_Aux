4169db4cbd081e2eef4cd05ef12841d890abf8ae
Ithias <ALDieguez9@gmail.com>
2025-05-26 03:45:06 -0500

    B2 limb adjustments

--- Factions/CIS/Infantry/config.cpp
+++ Factions/CIS/Infantry/config.cpp
@@ -215,6 +215,178 @@ class CfgVehicles {
         respawnMagazines[] = {"332nd_CIS_Wrist_Blaster_Mag", "332nd_CIS_Wrist_Blaster_Mag", "332nd_CIS_Wrist_Blaster_Mag", "332nd_CIS_Wrist_Blaster_Mag", "332nd_CIS_Wrist_Blaster_Mag", };
         linkedItems[] = {"ItemMap", "JLTS_droid_comlink", "ItemCompass", "ItemWatch","332nd_NVG_Chip"};
         respawnLinkedItems[] = {"ItemMap", "JLTS_droid_comlink", "ItemCompass", "ItemWatch","332nd_NVG_Chip"};
+		class HitPoints: HitPoints
+		{
+			class HitFace
+			{
+				armor = 1;
+				explosionShielding = 1.5;
+				material = -1;
+				minimalHit = 0.01;
+				name = "face_hub";
+				passThrough = 0.1;
+				radius = 0.08;
+			};
+			class HitNeck: HitFace
+			{
+				armor = 5;
+				explosionShielding = 1.5;
+				material = -1;
+				minimalHit = 0.01;
+				name = "neck";
+				passThrough = 0.1;
+				radius = 0.1;
+			};
+			class HitHead: HitNeck
+			{
+				armor = 1;
+				depends = "HitFace max HitNeck";
+				explosionShielding = 1.5;
+				material = -1;
+				minimalHit = 0.01;
+				name = "head";
+				passThrough = 0.1;
+				radius = 0.2;
+			};
+			class HitPelvis: HitHead
+			{
+				armor = 5;
+				depends = "";
+				explosionShielding = 2;
+				material = -1;
+				minimalHit = 0.01;
+				name = "pelvis";
+				passThrough = 0.1;
+				radius = 0.2;
+				visual = "injury_body";
+			};
+			class HitAbdomen: HitPelvis
+			{
+				armor = 5;
+				depends = "";
+				explosionShielding = 2;
+				material = -1;
+				minimalHit = 0.01;
+				name = "spine1";
+				passThrough = 0.1;
+				radius = 0.15;
+				visual = "injury_body";
+			};
+			class HitDiaphragm: HitAbdomen
+			{
+				armor = 5;
+				depends = "";
+				explosionShielding = 2;
+				material = -1;
+				minimalHit = 0.01;
+				name = "spine2";
+				passThrough = 0.1;
+				radius = 0.15;
+				visual = "injury_body";
+			};
+			class HitChest: HitDiaphragm
+			{
+				armor = 5;
+				depends = "";
+				explosionShielding = 2;
+				material = -1;
+				minimalHit = 0.01;
+				name = "spine3";
+				passThrough = 0.1;
+				radius = 0.15;
+				visual = "injury_body";
+			};
+			class HitBody: HitChest
+			{
+				armor = 1500;
+				depends = "HitPelvis max HitAbdomen max HitDiaphragm max HitChest";
+				explosionShielding = 2;
+				material = -1;
+				minimalHit = 0.01;
+				name = "body";
+				passThrough = 0.1;
+				radius = 0.16;
+				visual = "injury_body";
+			};
+			class HitArms: HitBody
+			{
+				armor = 5;
+				depends = "0";
+				explosionShielding = 2;
+				material = -1;
+				minimalHit = 0.01;
+				name = "arms";
+				passThrough = 0.1;
+				radius = 0.1;
+				visual = "injury_hands";
+			};
+			class HitHands: HitArms
+			{
+				armor = 5;
+				depends = "HitArms";
+				explosionShielding = 2;
+				material = -1;
+				minimalHit = 0.01;
+				name = "hands";
+				passThrough = 0.1;
+				radius = 0.1;
+				visual = "injury_hands";
+			};
+			class HitLegs: HitHands
+			{
+				armor = 5;
+				depends = "0";
+				explosionShielding = 2;
+				material = -1;
+				minimalHit = 0.01;
+				name = "legs";
+				passThrough = 0.1;
+				radius = 0.12;
+				visual = "injury_legs";
+			};
+			class Incapacitated: HitLegs
+			{
+				armor = 1000;
+				depends = "(((Total - 0.25) max 0) + ((HitHead - 0.25) max 0) + ((HitBody - 0.25) max 0)) * 2";
+				explosionShielding = 1;
+				material = -1;
+				minimalHit = 0;
+				name = "body";
+				passThrough = 1;
+				radius = 0;
+				visual = "";
+			};
+			class HitLeftArm
+			{
+				armor = 5;
+				explosionShielding = 2;
+				material = -1;
+				minimalHit = 0.01;
+				name = "hand_l";
+				passThrough = 0.1;
+				radius = 0.1;
+				visual = "injury_hands";
+			};
+			class HitRightArm: HitLeftArm
+			{
+				name="hand_r";
+			};
+			class HitLeftLeg
+			{
+				armor = 5;
+				explosionShielding = 2;
+				material = -1;
+				minimalHit = 0.01;
+				name = "leg_l";
+				passThrough = 0.1;
+				radius = 0.1;
+				visual = "injury_legs";
+			};
+			class HitRightLeg: HitLeftLeg
+			{
+				name="leg_r";
+			};	
+		};
     };
     class 332nd_Droid_B2_Rockets: 332nd_Droid_B2 
 	{
