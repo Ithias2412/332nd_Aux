136a5a505752ec7d4ee206b7f5cee184466d09c4
Ithias <ALDieguez9@gmail.com>
2025-08-14 23:29:18 -0500

    Just to piss you off

--- Factions/SubCats/config.cpp
+++ Factions/SubCats/config.cpp
@@ -106,9 +106,4 @@ class CfgEditorSubcategories
     {
         displayName = "[332nd] Resupply Crates";
     };
-    //Flags
-    class EdSubcat_332nd_Enian_Flags
-    {
-        displayName = "[332nd] East Enian Flags";
-    };
 };
