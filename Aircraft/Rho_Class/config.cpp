276dd1b76d62492e2179ef9a67a81dd5558b9592
Ithias <ALDieguez9@gmail.com>
2025-12-30 23:08:16 -0600

    Idk

--- Aircraft/Rho_Class/config.cpp
+++ Aircraft/Rho_Class/config.cpp
@@ -6,6 +6,7 @@ class CfgPatches
 		units[] =
         {
 			"332nd_Rho_Class",
+			"332nd_Transport_Rho_Crate",
         };
 		weapons[] =
         {
@@ -76,6 +77,7 @@ class CfgVehicles
 		ls_impulsor_fuelDrain_1=0.00005;
 		ls_impulsor_fuelDrain_2=0.00010;
 		armor = 400;
+		transportSoldier=34;
 		weapons[] =
 		{
 			"332nd_Rho_Cannon",
@@ -118,6 +120,52 @@ class CfgVehicles
 				};
 			};
 		};
+		class ACE_SelfActions: ACE_SelfActions
+		{
+			class TFAR_IntercomChannel
+			{
+				condition = "true";
+				displayName = "Intercom Channel";
+				icon = "";
+				statement = "";
+				class TFAR_IntercomChannel_1
+				{
+					condition = "_vehicle = vehicle ACE_Player; _intercom = _vehicle getVariable [format ['TFAR_IntercomSlot_%1',(netID ACE_Player)],-2]; if (_intercom == -2) then {_intercom = _vehicle getVariable ['TFAR_defaultIntercomSlot',TFAR_defaultIntercomSlot]}; _intercom != 0";
+					displayName = "Infantry";
+					statement = "(vehicle ACE_Player) setVariable [format ['TFAR_IntercomSlot_%1',(netID ACE_Player)],0,true];";
+				};
+				class TFAR_IntercomChannel_2
+				{
+					condition = "_vehicle = vehicle ACE_Player; _intercom = _vehicle getVariable [format ['TFAR_IntercomSlot_%1',(netID ACE_Player)],-2]; if (_intercom == -2) then {_intercom = _vehicle getVariable ['TFAR_defaultIntercomSlot',TFAR_defaultIntercomSlot]}; _intercom != 1";
+					displayName = "Crew";
+					statement = "(vehicle ACE_Player) setVariable [format ['TFAR_IntercomSlot_%1',(netID ACE_Player)],1,true];";
+				};
+				class TFAR_IntercomChannel_3
+				{
+					condition = "_vehicle = vehicle ACE_Player; _intercom = _vehicle getVariable [format ['TFAR_IntercomSlot_%1',(netID ACE_Player)],-2]; if (_intercom == -2) then {_intercom = _vehicle getVariable ['TFAR_defaultIntercomSlot',TFAR_defaultIntercomSlot]}; _intercom != 2";
+					displayName = "Command";
+					statement = "(vehicle ACE_Player) setVariable [format ['TFAR_IntercomSlot_%1',(netID ACE_Player)],2,true];";
+				};
+				class TFAR_IntercomChannel_disabled
+				{
+					condition = "_vehicle = vehicle ACE_Player; _intercom = _vehicle getVariable [format ['TFAR_IntercomSlot_%1',(netID ACE_Player)],-2]; if (_intercom == -2) then {_intercom = _vehicle getVariable ['TFAR_defaultIntercomSlot',TFAR_defaultIntercomSlot]}; _intercom != -1";
+					displayName = "Disabled";
+					statement = "(vehicle ACE_Player) setVariable [format ['TFAR_IntercomSlot_%1',(netID ACE_Player)],-1,true];";
+				};
+			};
+		};
+	};
+	class 3AS_Rho_Crate_Base_F;
+	class 3AS_Rho_Crate_REP_Transport: 3AS_Rho_Crate_Base_F
+	{
+		class ACE_SelfActions;
+	};
+	class 332nd_Transport_Rho_Crate: 3AS_Rho_Crate_REP_Transport
+	{
+		faction="332nd_Faction";
+		displayName = "[332nd] Rho Crate (Transport) [WIP]";
+		armor = 400;
+		transportSoldier=34;
 		class ACE_SelfActions: ACE_SelfActions
 		{
 			class TFAR_IntercomChannel
