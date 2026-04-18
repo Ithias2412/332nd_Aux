e7c61a06ac2cc6ada69a95cb26ae08a7a4c96dc9
dansatr <franswidjojo@gmail.com>
2026-04-18 19:51:53 +0700

    Bacta Spray 2x Bandage
    fixed the bacta spray because it reverted back to normal.

--- Medical/config.cpp
+++ Medical/config.cpp
@@ -188,6 +188,45 @@ class ace_medical_treatment
         };
         class 332nd_Bacta_Spray: 332nd_Acti_Bandage 
 		{
+            class Abrasion { effectiveness = 8; reopeningChance = 0.6; reopeningMinDelay = 800; reopeningMaxDelay = 1500; };
+            class AbrasionMinor: Abrasion { effectiveness = 8; };
+            class AbrasionMedium: Abrasion { effectiveness = 6; reopeningChance = 0.9; };
+            class AbrasionLarge: Abrasion { effectiveness = 5; reopeningChance = 1; };
+
+            class Avulsion: Abrasion { effectiveness = 4; reopeningChance = 0.7; reopeningMinDelay = 1000; reopeningMaxDelay = 1600; };
+            class AvulsionMinor: Avulsion { effectiveness = 4; };
+            class AvulsionMedium: Avulsion { effectiveness = 2.8; };
+            class AvulsionLarge: Avulsion { effectiveness = 2; };
+
+            class Contusion: Abrasion { effectiveness = 4; reopeningChance = 0; reopeningMinDelay = 0; reopeningMaxDelay = 0; };
+            class ContusionMinor: Contusion {};
+            class ContusionMedium: Contusion {};
+            class ContusionLarge: Contusion {};
+
+            class Crush: Abrasion { effectiveness = 4; reopeningChance = 0.5; reopeningMinDelay = 600; reopeningMaxDelay = 1000; };
+            class CrushMinor: Crush { effectiveness = 4; reopeningChance = 0.6; };
+            class CrushMedium: Crush { effectiveness = 3.4; reopeningChance = 0.7; };
+            class CrushLarge: Crush { effectiveness = 3.2; reopeningChance = 0.8; };
+
+            class Cut: Abrasion { effectiveness = 10; reopeningChance = 0.4; reopeningMinDelay = 700; reopeningMaxDelay = 1000; };
+            class CutMinor: Cut { effectiveness = 10; reopeningChance = 0.6; };
+            class CutMedium: Cut { effectiveness = 7; reopeningChance = 0.7; };
+            class CutLarge: Cut { effectiveness = 4; reopeningChance = 0.8; };
+
+            class Laceration: Abrasion { effectiveness = 4; reopeningChance = 0.65; reopeningMinDelay = 500; reopeningMaxDelay = 2000; };
+            class LacerationMinor: Laceration { effectiveness = 4; reopeningChance = 0.65; };
+            class LacerationMedium: Laceration { effectiveness = 3; reopeningChance = 0.8; };
+            class LacerationLarge: Laceration { effectiveness = 2; reopeningChance = 0.9; };
+
+            class VelocityWound: Abrasion { effectiveness = 4.4; reopeningChance = 1; reopeningMinDelay = 800; reopeningMaxDelay = 2000; };
+            class VelocityWoundMinor: VelocityWound { effectiveness = 4.4; };
+            class VelocityWoundMedium: VelocityWound { effectiveness = 3.5; };
+            class VelocityWoundLarge: VelocityWound { effectiveness = 3; };
+
+            class PunctureWound: Abrasion { effectiveness = 5; reopeningChance = 1; reopeningMinDelay = 1000; reopeningMaxDelay = 3000; };
+            class PunctureWoundMinor: PunctureWound { effectiveness = 5; };
+            class PunctureWoundMedium: PunctureWound { effectiveness = 4; };
+            class PunctureWoundLarge: PunctureWound { effectiveness = 3; };
 		};
     };
     class IV
